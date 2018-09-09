1fd5fec59dca8dab21bb6773a03f1fd7034466b2
Callum <92019337@mail.computerpower.ac.nz>
2018-09-10 11:16:55 +1200

    Testing

--- src/collectioninterfacetask/CollectionInterfaceTask.java
+++ src/collectioninterfacetask/CollectionInterfaceTask.java
@@ -3,8 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package collectioninterfacetask;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.LinkedList;
 
 /**
  *
@@ -12,11 +16,51 @@ package collectioninterfacetask;
  */
 public class CollectionInterfaceTask {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void pntbr() {
+        System.out.println("========================================");
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        ArrayList array1 = new ArrayList();
+        array1.add("3.14");
+        array1.add("2.72");
+        array1.add("1.13");
+        System.out.println(array1.size());
+        System.out.println(array1);
+        System.out.println(array1.contains("3.14"));
+        System.out.println(array1.remove("3.14"));
+        System.out.println(array1);
+        array1.clear();
+        System.out.println(array1);
+
+        pntbr();
+
+        Stack stack1 = new Stack();
+        stack1.add("3.14");
+        stack1.add("2.72");
+        stack1.add("1.13");
+        System.out.println(stack1.size());
+        System.out.println(stack1);
+        System.out.println(stack1.contains("3.14"));
+        System.out.println(stack1.remove("3.14"));
+        System.out.println(stack1);
+        stack1.clear();
+        System.out.println(stack1);
+
+        pntbr();
+
+        LinkedList linkedList1 = new LinkedList();
+        linkedList1.add("3.14");
+        linkedList1.add("2.72");
+        linkedList1.add("1.13");
+        System.out.println(linkedList1.size());
+        System.out.println(linkedList1);
+        System.out.println(linkedList1.contains("3.14"));
+        System.out.println(linkedList1.remove("3.14"));
+        System.out.println(linkedList1);
+        linkedList1.clear();
+        System.out.println(linkedList1);
     }
 
 }
